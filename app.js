9fb39b827c9c9ce5155ce0a99eeef018cdc701e5
zx2a17 <48633832+zx2a17@users.noreply.github.com>
2024-07-15 09:08:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     // set response content    
         res.send(`<html>
                     <body>
-                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: Finanly Finished - this is now triggered by GHA!!</h1>
+                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: This is to check if old stuff works!!</h1>
                         <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
                             <img src="https://picsum.photos/400/400?random=1">
                         </div>
